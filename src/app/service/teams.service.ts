2b421fe3f9a207ec1e9d84f7dc49721546771b17
AlexisAbrate <abrate.alexis@outlook.fr>
2022-05-12 13:40:23 +0200

    ajout de graph dans team

--- src/app/service/teams.service.ts
+++ src/app/service/teams.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-//import { environment } from 'src/environments/environment';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
+//import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -48,6 +48,19 @@ export class TeamsService {
     return rep
   }
 
+  private _url_nation = environment.apiUrl+'team/players/nationality/teamid/'
+
+  getNationality(id: Number, season: Number) {
+    var rep = this._http.get(this._url_nation+id+"/"+season)
+    return rep
+  }
+
+  private _url_minutes = environment.apiUrl+'team/stats/minutes/'
+
+  getMinutes(id: Number, season: Number) {
+    var rep = this._http.get(this._url_minutes+season+"/"+id)
+    return rep
+  }
   
   
 
--- src/app/teams/team/team.component.ts
+++ src/app/teams/team/team.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TeamsService } from 'src/app/service/teams.service';
 import { ActivatedRoute } from '@angular/router';
+import { Chart } from 'chart.js';
 
 
 
@@ -22,9 +23,11 @@ export class TeamComponent implements OnInit {
   data: any
   goals: [] | any 
   minutes: [] | any 
-
+  pie_min: [] | any
   lineup: [] | any
-
+  nationality : [] | any
+  bar_fixtures: [] | any
+ polar_min: [] | any
   //monoChartTypes : ChartType[] = [  'bar' , 'pie' , 'doughnut' ,'polarArea' ];
   //multiChartTypes : ChartType[] = [ 'line' , 'bar'  ];
 
@@ -78,29 +81,121 @@ export class TeamComponent implements OnInit {
         this.venue = this.response_infos.venue;
       })
       this.service.getFixtures(this._id).subscribe(data => {
-          this.fixture = data
+          this.fixture = data        
           console.log(this.fixture)
         });
 
         this.service.getPlayers(this._id).subscribe(data => {
           this.players = data
+
+          var dataMinutes = []
+          var dataPlayer = []
+          for(let play of this.players) {
+            dataPlayer.push(play.player.name)
+            dataMinutes.push(play.statistics[0].games.minutes)
+          }
+
+          this.pie_min = new Chart('pie_min', {
+            type : 'pie',
+            data: {
+              labels: dataPlayer,
+              datasets : [
+                {
+                  label: "Minutes jouées",
+                  data: dataMinutes
+                }
+              ]
+            },
+            options: {
+              responsive: true,
+              plugins: {
+                title: {
+                  display: true,
+                  text: "Nombre de minutes jouées dans l'année par un joueur"
+                }
+              }
+            }
+          })
+          
         })
+
+
+        
 
         
         this.service.getStats(this._id,2021).subscribe(data => {
           this.stats = data
           this.goals = [this.stats.goals.for.total.total,this.stats.goals.for.total.away,this.stats.goals.for.total.home]
-                        
+           
+          var dataButs = []
+          dataButs.push(this.stats.fixtures.played.total,this.stats.fixtures.wins.total,this.stats.fixtures.draws.total,this.stats.fixtures.loses.total)
+          console.log(dataButs)
+          var labelButs = ["Match joués","Victoires","Match nul", "Defaites"]  
+          this.bar_fixtures= new Chart('bar_fix', {
+            type : 'bar',
+            data: {
+              labels: labelButs,
+              datasets : [
+                {
+                  label: "Nombre de match",
+                  data: dataButs
+                }
+              ]
+            },
+            options: {
+              responsive: true,
+              plugins: {
+                title: {
+                  display: true,
+                  text: "Stats sur les matchs joués dans l'année"
+                }
+              }
+            }
+          })
         })
 
-
-        /*
-        this.service.getMinutes(this._id,2021).subscribe(data => {
-          this.minutes = data
-          this.minutesChartData = [
-            { data: this.minutes, label: 'Buts' },
-          ];          
+        /*this.service.getNationality(this._id,2021).subscribe(data => {
+          this.nationality = data
+          var labelChart = []
+          var dataChart = []
+          for(let nation of)
         })*/
+
+        
+        this.service.getMinutes(this._id,2021).subscribe(data => {
+            this.minutes = data
+            console.log("Ceci : "+ this.minutes)
+             var labelPolar = ["0-15","16-30","31-45","46-60","61-75","76-90","91-105"]
+             var valuePolar = []
+             for(let min of this.minutes) {
+               valuePolar.push(min.total)
+             }
+
+             console.log(valuePolar)
+
+             this.polar_min= new Chart('polar_min', {
+              type : 'polarArea',
+              data: {
+                labels: labelPolar,
+                datasets : [
+                  {
+                    label: "Nombre de buts",
+                    data: valuePolar
+                  }
+                ]
+              },
+              options: {
+                responsive: true,
+                plugins: {
+                  title: {
+                    display: true,
+                    text: "But selon les minutes"
+                  }
+                }
+              },
+            })
+
+        })
        
         
 
